106d18f8634b40533289e0f6a42af2e8e087f3c1
Diego-Morales-30 <diegollavemorales@gmail.com>
2024-07-05 19:43:12 -0500

    Combo Simple

--- controller/ComboSimpleController.py
+++ controller/ComboSimpleController.py
@@ -4,5 +4,23 @@ class ComboSimpleController:
     def __init__(self) -> None:
         app = QtWidgets.QApplication([])
         self.ventana = uic.loadUi("view/frm_lista_desplegables.ui")
+        self.agregarElementos()
+        self.ventana.btnver.clicked.connect(self.verResultado)
         self.ventana.show()
         app.exec()
+    
+    def agregarElementos(self):
+        self.ventana.cbopais.addItem("Perú", "PA01")
+        self.ventana.cbopais.addItem("Argentina", "PA02")
+        self.ventana.cbopais.addItem("Brasil", "PA03")
+        self.ventana.cbopais.addItem("Uruguay", "PA04")
+        paises = ["Chile", "Ecuador", "Venezuela"]
+        self.ventana.cbopais.addItems(paises)
+    
+    def verResultado(self):
+        textpais = self.ventana.cbopais.currentText()
+        codpais = self.ventana.cbopais.currentData()
+        index = self.ventana.cbopais.currentIndex()
+        self.ventana.lblresultado.setText(textpais + 
+                                          "-" + str(index))
+    
